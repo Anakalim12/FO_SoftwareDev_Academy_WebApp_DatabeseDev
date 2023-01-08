ba4ac88ce20fbcd45fae7ede4a5f3720a805d550
Mesut Sara <mesuttsara@gmail.com>
2023-01-08 16:45:18 +0300

    Odev_1/Case_1 için program.cs dosyası eklendi

---
+++ Weeks/Week_2/Odev_1/Case_1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Case_1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
+        Console.WriteLine("Lütfen dizi boyutu için bir sayı giriniz:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] dizi = new int[n];
+        Console.WriteLine("Lütfen " + n + " adet sayı girişi yapınız:");
+        Console.WriteLine("Girilen sayılar 0'dan büyük olmalıdır!");
+        for (int i = 0; i < dizi.Length; i++)
+        {
+            dizi[i]= Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine("Girdiğiniz sayılardan çift olanlar:");
+        foreach (int sayi in dizi)
+        {
+            if (sayi %2 == 0){
+                
+                Console.WriteLine(sayi);
+            }
+        }
+    }
+}
