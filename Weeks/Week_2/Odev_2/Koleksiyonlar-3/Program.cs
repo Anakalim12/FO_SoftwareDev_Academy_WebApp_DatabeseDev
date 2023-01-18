bf69f134b19d34f907b91868f46fa5733215ca43
Mesut Sara <mesuttsara@gmail.com>
2023-01-18 22:44:08 +0300

    Koleksiyonlar ödev-3 için program.cs dosyası eklendi

---
+++ Weeks/Week_2/Odev_2/Koleksiyonlar-3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace Koleksiyonlar_3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+
+        ArrayList sesliHarfler = new ArrayList();
+        Console.WriteLine("Lütfen bir metin/cümle yazınız");
+        string sentence = Console.ReadLine();
+        
+
+        foreach (char item in sentence.ToLower())
+        {
+            if (item == 'a' || item == 'e' || item == 'ı' || item == 'o' || item == 'u' || item == 'i' || item == 'ö' || item == 'ü')
+            {
+                sesliHarfler.Add(item);
+            }
+        }
+        sesliHarfler.Sort();
+        foreach (var item in sesliHarfler)
+        {
+            Console.WriteLine(item);
+        }
+        
+        
+        
+    }
+}
